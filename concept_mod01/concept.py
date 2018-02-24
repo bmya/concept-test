21c881029525b3aad508fb38f24dfb5e89914e50
Daniel Blanco <daniel@blancomartin.cl>
2018-02-24 20:43:18 -0300

    first

---
+++ concept_mod01/concept.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+import api2
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api2.localization('es', True)
+    def action_invoice_open(self):
+        raise UserError('CONCEPT 01 - es')
---
+++ concept_mod02/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Localization Concept Test 02",
+    'version': '10.0.1.0.0',
+    'category': 'Miscellaneous',
+    'author':  'BMyA SA - Blanco Martín & Asociados',
+    'website': 'http://blancomartin.cl',
+    'license': 'LGPL-3',
+    'summary': '',
+    'description': """
+Proof of concept 02
+""",
+    'depends': [
+        'account',
+        ],
+    'external_dependencies': {
+        'python': [
+            'api2',
+        ]
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
